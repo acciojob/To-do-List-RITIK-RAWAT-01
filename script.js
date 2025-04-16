7e2280ade148d305c8247679358c7dfd06e0e3af
RITIK-RAWAT-01 <ritvikrawat35100@gmail.com>
2025-04-16 15:42:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 //your code here
 
+let input = document.getElementById('newTodoInput');
+let btn = document.getElementById('addTodoBtn');
+let todoList = document.getElementById('todoList')
+
+btn.addEventListener('click',() => {
+	let li = document.createElement('li')
+	li.innerHTML = input.value
+	todoList.append(li)
+	input.value = ""
+})
